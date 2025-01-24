57b147c41a5df32bf5569a7612034892c2fb15c6
martin <martin.zivojinovic@etu.u-bordeaux.fr>
2025-01-24 13:25:28 +0100

    refacto

--- zipiler/src/zipiler/prog_manager/prog_manager.rs
+++ zipiler/src/zipiler/prog_manager/prog_manager.rs
@@ -1,4 +1,3 @@
-
 use crate::zipiler::tokenizer::include::MemZone;
 
 use super::include::*;
@@ -9,7 +8,7 @@ pub struct ProgManager {
     /// Link var name to a stack of variable address on the stack. The top is the good one
     pub var_name_map: HashMap<String, Stack<usize>>,
     /// Link var address to a stack of variable definition. The top is the good one, two variable can have the
-    /// same address if there are not on the same stage 
+    /// same address if there are not on the same stage
     pub var_map: HashMap<usize, Stack<VariableDefinition>>,
     /// Link name to a stack of func address on the mem prog
     pub func_name_map: HashMap<String, Stack<usize>>,
@@ -30,20 +29,19 @@ pub struct ProgManager {
     /// Contains the address of the function we are currently defining
     pub current_func: Option<usize>,
     /// Link a type id (index to access the vec) to type name
-    pub titn: Vec::<String>,
+    pub titn: Vec<String>,
     /// Link a type name to his id and his size. For a class the size gonna be the size of a pointer, the
     /// fields of the class are stored on the heap
-    pub tnti: HashMap<String, (u8, usize)>,  // Size, id
+    pub tnti: HashMap<String, (u8, usize)>, // Size, id
     /// The current stage
     pub stage: u32,
     /// Name of the class we are currently defining
     pub current_class: String,
     /// The current line of the script.
-    line_number: u64
+    line_number: u64,
 }
 
 impl ProgManager {
-
     pub fn new() -> ProgManager {
         ProgManager {
             class_name_map: HashMap::new(),
@@ -62,62 +60,75 @@ impl ProgManager {
             current_func: None,
             stage: 0,
             current_class: String::new(),
-            line_number: 1
+            line_number: 1,
         }
     }
 
-    
-
     pub fn _affect_to(&self, addr: usize) -> String {
         let size = self.get_var_def(&addr).unwrap().type_var().size() as usize;
-        format!("\nmov {}[_stack + {STACK_REG} + {}], {}", ASM_SIZES[size], addr, RAX_SIZE[size])
+        format!(
+            "\nmov {}[_stack + {STACK_REG} + {}], {}",
+            ASM_SIZES[size], addr, RAX_SIZE[size]
+        )
     }
 
     pub fn affect_to_wsize(&self, addr: usize, size: usize, val: usize, add_r15: bool) -> String {
         if add_r15 {
-            format!("
+            format!(
+                "
 mov rax, r15
 add rax, {}
-mov {}[_stack + {STACK_REG} + {}], {}", val, ASM_SIZES[size], addr, RAX_SIZE[size])        
-        }else {
-            format!("\nmov {}[_stack + {STACK_REG} + {}], {}", ASM_SIZES[size], addr, val)        
+mov {}[_stack + {STACK_REG} + {}], {}",
+                val, ASM_SIZES[size], addr, RAX_SIZE[size]
+            )
+        } else {
+            format!(
+                "\nmov {}[_stack + {STACK_REG} + {}], {}",
+                ASM_SIZES[size], addr, val
+            )
         }
     }
 
     pub fn deref_var(&self, size: usize, stars: i32, spot: MemZone) -> String {
         if stars > 0 {
             match spot {
-                MemZone::Heap => format!("mov {}, {}[_heap + rax]", RAX_SIZE[size], ASM_SIZES[size]),
+                MemZone::Heap => {
+                    format!("mov {}, {}[_heap + rax]", RAX_SIZE[size], ASM_SIZES[size])
+                }
                 MemZone::Stack => {
                     let mut res = String::new();
                     for _ in 0..stars {
-                        res.push_str(&format!("\nmov {}, {}[_stack+rax]", RAX_SIZE[size], ASM_SIZES[size]))
+                        res.push_str(&format!(
+                            "\nmov {}, {}[_stack+rax]",
+                            RAX_SIZE[size], ASM_SIZES[size]
+                        ))
                     }
                     res
                 }
             }
-        }else{
+        } else {
             String::new()
         }
     }
 
-    
-
     pub fn handle_arg(&mut self, f: &Function, stars: i32, nth: usize) -> Result<String, String> {
         if f.args()[nth as usize].stars() as i32 != stars {
-            return Err("Not the good type for the call.".to_string())
+            return Err("Not the good type for the call.".to_string());
         }
         let size = self.get_type_size(stars, &f.args()[nth as usize].name()) as usize;
-        let res = format!("
+        let res = format!(
+            "
 pop rax
-mov {}[_stack + {STACK_REG} + {}], {}", ASM_SIZES[size], self.si(), RAX_SIZE[size]);
+mov {}[_stack + {STACK_REG} + {}], {}",
+            ASM_SIZES[size],
+            self.si(),
+            RAX_SIZE[size]
+        );
         self.stack_index += size;
         Ok(res)
     }
 
-    pub fn end_prog(&self) {
-        println!("{} {}", self.si(), self.hi())
-    }
+    pub fn end_prog(&self) {}
 
     pub fn is_in_func(&self) -> bool {
         self.current_func.is_some()
@@ -132,9 +143,13 @@ mov {}[_stack + {STACK_REG} + {}], {}", ASM_SIZES[size], self.si(), RAX_SIZE[siz
     }
 
     pub fn panic_bad_token(&self, receiver: &str, token: Token) {
-        panic!("Line {}: Unknow token type for a {receiver}: {:?}    {}", self.line_number(), token.token_type, token.content)
+        panic!(
+            "Line {}: Unknow token type for a {receiver}: {:?}    {}",
+            self.line_number(),
+            token.token_type,
+            token.content
+        )
     }
-
 }
 
 fn build_base_type_vec() -> Vec<String> {
@@ -148,5 +163,3 @@ fn build_tab_size_map() -> HashMap<String, (u8, usize)> {
     res.insert(String::from("void"), (0, 2));
     res
 }
-
-
